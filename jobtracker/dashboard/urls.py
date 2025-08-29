c7dd6f341eade6d6ad0575b446a052fcbb6cd8e6
JBailey273 <jbailey@squire.enterprises>
2025-08-28 21:55:03 -0400

    Add dashboard app with contractor and project views

---
+++ jobtracker/dashboard/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'dashboard'
+
+urlpatterns = [
+    path('', views.contractor_summary, name='contractor_summary'),
+    path('projects/', views.project_list, name='project_list'),
+    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
+]
