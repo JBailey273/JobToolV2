7a227442daeb37da13139ad54ebaece81dce20ff
JBailey273 <jbailey@squire.enterprises>
2025-08-29 11:00:46 -0400

    Improve login styling and handle missing settings

--- jobtracker/tracker/context_processors.py
+++ jobtracker/tracker/context_processors.py
@@ -1,5 +1,24 @@
+"""Context processors used across templates."""
+
+from django.db.utils import OperationalError, ProgrammingError
+
 from .models import GlobalSettings
 
 
 def global_settings(request):
-    return {'global_settings': GlobalSettings.objects.first()}
+    """Provide global settings without breaking if the DB is unavailable.
+
+    During initial setup or when migrations have not yet been applied the
+    ``tracker_globalsettings`` table may not exist. Previously this raised
+    an ``OperationalError`` which in turn caused a 500 error whenever a
+    template tried to access the login page. By gracefully handling these
+    database errors we can still render pages such as the login page and the
+    rest of the site will work once migrations are applied.
+    """
+
+    try:
+        settings_obj = GlobalSettings.objects.first()
+    except (OperationalError, ProgrammingError):
+        settings_obj = None
+
+    return {"global_settings": settings_obj}
