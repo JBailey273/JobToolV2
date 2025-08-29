eab7f1a88d4a179ca74ac8a4448e5523318d6c39
JBailey273 <jbailey@squire.enterprises>
2025-08-28 21:34:51 -0400

    Register ContractorUser in admin and add email backend

--- jobtracker/tracker/models.py
+++ jobtracker/tracker/models.py
@@ -1,11 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class Contractor(models.Model):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
     logo = models.CharField(max_length=255, blank=True)
     material_markup = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    def __str__(self) -> str:
+        return self.email
+
+
+class ContractorUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class ContractorUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    contractor = models.ForeignKey(Contractor, related_name='users', on_delete=models.CASCADE, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = ContractorUserManager()
 
     def __str__(self) -> str:
         return self.email
