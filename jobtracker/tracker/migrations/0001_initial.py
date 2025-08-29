731e393a9bc5c91a62ee7265109b2d40908b5cf1
JBailey273 <jbailey@squire.enterprises>
2025-08-28 22:53:34 -0400

    Consolidate user model into initial migration

--- jobtracker/tracker/migrations/0001_initial.py
+++ jobtracker/tracker/migrations/0001_initial.py
@@ -1,11 +1,15 @@
 from django.db import migrations, models
 import django.db.models.deletion
+import django.utils.timezone
+import tracker.models
 
 
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = []
+    dependencies = [
+        ('auth', '0012_alter_user_first_name_max_length'),
+    ]
 
     operations = [
         migrations.CreateModel(
@@ -13,9 +17,39 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=254, unique=True)),
-                ('password', models.CharField(max_length=128)),
-                ('logo', models.CharField(blank=True, max_length=255)),
+                ('logo', models.ImageField(upload_to='contractor_logos/', blank=True, null=True)),
                 ('material_markup', models.DecimalField(decimal_places=2, default=0, max_digits=5)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='GlobalSettings',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('logo', models.ImageField(upload_to='global_logos/', blank=True, null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ContractorUser',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('first_name', models.CharField(blank=True, max_length=150, verbose_name='first name')),
+                ('last_name', models.CharField(blank=True, max_length=150, verbose_name='last name')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('email', models.EmailField(max_length=254, unique=True)),
+                ('contractor', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='users', to='tracker.contractor')),
+                ('groups', models.ManyToManyField(blank=True, related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+            ],
+            options={
+                'abstract': False,
+            },
+            managers=[
+                ('objects', tracker.models.ContractorUserManager()),
             ],
         ),
         migrations.CreateModel(
