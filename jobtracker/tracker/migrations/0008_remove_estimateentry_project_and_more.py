089fdce7f8e6598c077f6c0c6c83d4bfcdd395ab
JBailey273 <jbailey@squire.enterprises>
2025-09-04 16:52:28 -0400

    Handle existing Estimate table

--- jobtracker/tracker/migrations/0008_remove_estimateentry_project_and_more.py
+++ jobtracker/tracker/migrations/0008_remove_estimateentry_project_and_more.py
@@ -18,10 +18,13 @@ def forward_migrate_estimates(apps, schema_editor):
     projects = Project.objects.filter(estimate_entries__isnull=False).distinct()
 
     for project in projects:
-        estimate = Estimate.objects.create(
+        estimate, _ = Estimate.objects.get_or_create(
             contractor=project.contractor,
             name=project.name,
-            created_date=project.start_date or django.utils.timezone.now().date(),
+            defaults={
+                "created_date": project.start_date
+                or django.utils.timezone.now().date(),
+            },
         )
         EstimateEntry.objects.filter(project=project).update(estimate=estimate)
         project.end_date = project.end_date or django.utils.timezone.now().date()
@@ -62,6 +65,7 @@ class Migration(migrations.Migration):
                     ),
                 ),
             ],
+            if_not_exists=True,
         ),
         migrations.AddField(
             model_name="estimateentry",
