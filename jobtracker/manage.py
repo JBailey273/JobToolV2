45e14300e5311001b7389a6b9df44d939a90077b
JBailey273 <jbailey@squire.enterprises>
2025-12-01 22:40:32 -0500

    Run migrations automatically for local server startup

--- jobtracker/manage.py
+++ jobtracker/manage.py
@@ -6,10 +6,14 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobtracker.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management import call_command
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment variable? Did you forget to activate a virtual environment?"
         ) from exc
+
+    if sys.argv[1:2] == ['runserver']:
+        call_command('migrate', interactive=False)
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
